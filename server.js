926b45c85ef573ac62f80d6426eb62dc2af22f1c
hayate_y <hayate.bgu.senyo@gmail.com>
2022-08-22 21:23:21 +0900

    [Add] Express 初期設定

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('nodejs')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
+
+app.listen(PORT, () => console.log('server running'))
