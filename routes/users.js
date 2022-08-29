6924f7d6da80276c1d10eb0a10f4cb4e9d984f9d
hayate_y <hayate.bgu.senyo@gmail.com>
2022-08-29 21:17:11 +0900

    [Add] ユーザーアンフォローAPIの実装

--- routes/users.js
+++ routes/users.js
@@ -73,6 +73,35 @@ router.put('/:id/follow', async (req, res) => {
   }
 });
 
+// Unfollow user
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          }
+        })
+        await currentUser.updateOne({
+          $pull: {
+            followings: req.params.id,
+          }
+        })
+        return res.status(200).json('Successfully Unfollowed')
+      } else {
+        return res.status(403).json('Already unfollowing');
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json('Can not unfollow yourself');
+  }
+});
+
 // router.get('/', (req, res) => {
 //   res.send('user router');
 // });
