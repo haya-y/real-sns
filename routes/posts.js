bdab067288f65c14217a613718b816b151ddc08a
hayate_y <hayate.bgu.senyo@gmail.com>
2022-09-05 21:07:41 +0900

    [Add] 「いいね」機能・タイムラインのAPIの実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 // create a post
 router.post('/', async (req, res) => {
@@ -20,9 +21,9 @@ router.put('/:id', async (req, res) => {
       await post.updateOne({
         $set: req.body,
       });
-      return res.status(200).json('Successfully posted')
+      return res.status(200).json('Successfully posted');
     } else {
-      return res.status(403).json('You can only update your posts.')
+      return res.status(403).json('You can only update your posts.');
     }
   } catch (err) {
     return res.status(403).json(err);
@@ -35,9 +36,9 @@ router.delete('/:id', async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
       await post.deleteOne();
-      return res.status(200).json('Your post was successfully deleted')
+      return res.status(200).json('Your post was successfully deleted');
     } else {
-      return res.status(403).json('You can only delete existing post')
+      return res.status(403).json('You can only delete existing post');
     }
   } catch (err) {
     return res.status(403).json(err);
@@ -48,12 +49,53 @@ router.delete('/:id', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    return res.status(200).json(post)
+    return res.status(200).json(post);
   } catch (err) {
-    return res.status(403).json(err)
+    return res.status(403).json(err);
   }
-})
+});
 
 // add like to a particular post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // if the post does not have like
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json('Successfully put like on the post');
+    } else {
+      // if the post already has like
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(403).json('Successfully removed like from post');
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// get posts of timeline
+router.get('/timeline/all', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    // get all posts of following friends
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      }),
+    );
+    return res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
