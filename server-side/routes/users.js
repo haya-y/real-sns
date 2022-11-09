21ae4eed0c32af7a43597d2f115f62231439671f
hayate_y <hayate.bgu.senyo@gmail.com>
2022-11-09 21:12:58 +0900

    [Fix] ユーザー更新APIの修正

--- server-side/routes/users.js
+++ server-side/routes/users.js
@@ -6,10 +6,10 @@ const User = require('../models/User');
 router.put('/:id', async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user = await User.findOneAndUpdate(req.params.id, {
+      const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
-      res.status(200).json('Updated user info');
+      return res.status(200).json('Updated user info');
     } catch (err) {
       return res.status(500).json(err);
     }
