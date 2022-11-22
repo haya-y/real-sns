2d67e96a2c2161373eeb3f284e964dbfc22cd979
hayate_y <hayate.bgu.senyo@gmail.com>
2022-11-22 21:59:00 +0900

    [Add] ログインAPIの実行確認

--- client-side/src/pages/login/Login.jsx
+++ client-side/src/pages/login/Login.jsx
@@ -1,16 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useContext, useRef } from 'react';
+import { loginCall } from '../../actionCalls';
+import { AuthContext } from '../../state/AuthContext';
 import './Login.css';
 
 export default function Login() {
   const email = useRef('');
   const password = useRef('');
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email.current.value);
-    console.log(password.current.value);
+    // console.log(email.current.value);
+    // console.log(password.current.value);
+    loginCall(
+      {
+        email: email.current.value,
+        password: password.current.value,
+      },
+      dispatch,
+      );
+      console.log({user});
   };
 
+  
   return (
     <div className='login'>
       <div className='loginWrapper'>
