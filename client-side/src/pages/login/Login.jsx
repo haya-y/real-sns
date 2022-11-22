0a615e2022e8741241544e92100f731f7b7dc5e8
hayate_y <hayate.bgu.senyo@gmail.com>
2022-11-22 20:38:52 +0900

    [Add] Loginコンポーネントの作成

--- client-side/src/pages/login/Login.jsx
+++ client-side/src/pages/login/Login.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Login.css';
 
 export default function Login() {
+  const email = useRef('');
+  const password = useRef('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('チェック');
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
+
   return (
     <div className='login'>
       <div className='loginWrapper'>
@@ -9,15 +19,22 @@ export default function Login() {
           <h3 className='loginLogo'>Real SNS</h3>
           <span className='loginDesc'>本格的なSNSを、自分の手で</span>
         </div>
-        <div className='loginRight'>
-          <div className='loginBox'>
+        <div className='loginRight' onSubmit={(e) => handleSubmit(e)}>
+          <form className='loginBox'>
             <p className='loginMsg'>ログインはこちら</p>
-            <input type='text' className='loginInput' placeholder='Eメール' />
-            <input type='text' className='loginInput' placeholder='パスワード' />
-          <button className='loginButton'>ログイン</button>
-          <span className='loginForgot'>パスワードを忘れた方へ</span>
-          <button className='loginRegisterButton'>アカウント作成</button>
-          </div>
+            <input type='email' className='loginInput' placeholder='Eメール' required ref={email} />
+            <input
+              type='password'
+              className='loginInput'
+              placeholder='パスワード'
+              required
+              minLength={6}
+              ref={password}
+            />
+            <button className='loginButton'>ログイン</button>
+            <span className='loginForgot'>パスワードを忘れた方へ</span>
+            <button className='loginRegisterButton'>アカウント作成</button>
+          </form>
         </div>
       </div>
     </div>
