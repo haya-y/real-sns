483dbb6f0412fe3472006513d1dd4f0cfc3159c3
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-02 01:13:43 +0900

    [Mod] Topbarをスタイルドコンポーネント・BEM記法で書き換え

--- client-side/src/components/topbar/Topbar.tsx
+++ client-side/src/components/topbar/Topbar.tsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { Chat, Notifications, Search } from '@mui/icons-material';
-import './Topbar.css';
-import {Link} from 'react-router-dom';
+import { Chat as MUIChat, Notifications as MUINotifications, Search as MUISearch } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { StyledTopbarDiv } from './Topbar.styles';
 
 export default function Topbar() {
   return (
-    <div className="topbarContainer">
-      <div className="topbarLeft">
-      <Link to={'/'} style={{ textDecoration: 'none'}}>
-        <span className="log">Real SNS</span>
-      </Link>
+    <StyledTopbarDiv>
+      <div className='topbarLeft'>
+        <Link to={'/'} style={{ textDecoration: 'none' }}>
+          <span className='topbarLeft-log'>Real SNS</span>
+        </Link>
       </div>
-      <div className="topbarCenter">
-        <div className="searchbar">
-          <Search className="searchIcon"></Search>
-          <input type="text" className="searchInput" placeholder="探し物は何ですか？" />
+      <div className='topbarCenter'>
+        <div className='topbarCenter-searchbar'>
+          <MUISearch className='topbarCenter-searchbar-searchIcon'></MUISearch>
+          <input type='text' className='topbarCenter-searchbar-searchInput' placeholder='探し物は何ですか？' />
         </div>
       </div>
-      <div className="topbarRight">
-        <div className="topbarItemIcons">
-          <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">1</span>
+      <div className='topbarRight'>
+        <div className='topbarRight-icons'>
+          <div className='topbarRight-icons-items'>
+            <MUIChat />
+            <span className='topbarRight-icons-items-badge'>10</span>
           </div>
-          <div className="topbarIconItem">
-            <Notifications />
-            <span className="topbarIconBadge">2</span>
+          <div className='topbarRight-icons-items'>
+            <MUINotifications />
+            <span className='topbarRight-icons-items-badge'>20</span>
           </div>
-          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+          <img src='/assets/person/1.jpeg' alt='' />
         </div>
       </div>
-    </div>
+    </StyledTopbarDiv>
   );
 }
