5134b3a29424e4bfea120e30284e7ec9adbfb430
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-02 00:49:30 +0900

    [Mod] Homeをスタイルドコンポーネントで作り直し

---
+++ client-side/src/pages/home/Home.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledHomeDiv = styled.div`
+  .homeContainer {
+    display: flex;
+    width: 100%;
+    background-color: #f8fcff;
+  }
+`;
