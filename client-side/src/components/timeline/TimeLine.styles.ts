ccca634dc2513fda246c767fd6b4ba0cf45693ce
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-02 16:29:53 +0900

    [Mod] TimeLine・Shareをスタイルドコンポーネント・BEM記法で書き換え

---
+++ client-side/src/components/timeline/TimeLine.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledTimeLineDiv = styled.div`
+  flex: 6;
+
+  .timelineWrapper {
+    padding: 20px;
+  }
+`;
--- client-side/src/components/timeline/TimeLine.tsx
+++ client-side/src/components/timeline/TimeLine.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
-import "./TimeLine.css";
+import "./TimeLine.styles";
+import { StyledTimeLineDiv } from './TimeLine.styles';
 
 type Props = {
   username?: any
@@ -12,23 +13,23 @@ export default function TimeLine({ username }: Props) {
   const [posts, setPosts] = useState<any[]>([]);
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchUserPosts = async () => {
       const response = username
         ? await axios.get(`/posts/profile/${username}`)
         : await axios.get("/posts/timeline/6304d19040d4092261cbeea3");
       setPosts(response.data);
     };
-    fetchPosts();
+    fetchUserPosts();
   }, [username]);
 
   return (
-    <div className='timeline'>
+    <StyledTimeLineDiv>
       <div className='timelineWrapper'>
         <Share />
         {posts.map((post) => (
           <Post post={post} key={post._id} />
         ))}
       </div>
-    </div>
+    </StyledTimeLineDiv>
   );
 }
