ebb3c79910de80ebae1e11c43a5b4a6f083b75a9
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-10 04:06:38 +0900

    [Mod] Profileをスタイルドコンポーネント・BEM記法で書き換え

--- client-side/src/pages/profile/Profile.tsx
+++ client-side/src/pages/profile/Profile.tsx
@@ -3,14 +3,17 @@ import React, { useEffect, useState } from 'react';
 import { Sidebar } from '../../components/sidebar/Sidebar';
 import { TimeLine } from '../../components/timeline/TimeLine';
 import { Topbar } from '../../components/topbar/Topbar';
-import './Profile.css';
 import { useParams } from 'react-router-dom';
 import { Rightbar } from '../../components/rightbar/Rightbar';
+import { User } from '../../type/User.types';
+import { Users } from '../../dummyData';
+import { StyledProfileDiv } from './Profile.styles';
 
 export const Profile = () => {
-  const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+  // const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+  const PUBLIC_FOLDER = './assets';
 
-  const [user, setUser] = useState<any>({});
+  const [user, setUser] = useState<User>(Users[0]);
   const username = useParams().username;
   console.log({ username });
 
@@ -24,31 +27,31 @@ export const Profile = () => {
   }, [username]);
 
   return (
-    <>
+    <StyledProfileDiv>
       <Topbar />
       <div className='profile'>
         <Sidebar />
-        <div className='profileRight'>
-          <div className='profileRightTop'>
-            <div className='profileCover'>
-              <img src={user.coverPicture || PUBLIC_FOLDER + '/post/3.jpeg'} alt='' className='profileCoverImg' />
+        <div className='profile-right'>
+          <div className='profile-right-top'>
+            <div className='profile-right-top-cover'>
+              <img src={user.coverPicture || PUBLIC_FOLDER + '/post/3.jpeg'} alt='' className='profile-right-top-cover-coverImg' />
               <img
                 src={user.profilePicture || PUBLIC_FOLDER + '/person/noAvatar.png'}
                 alt=''
-                className='profileUserImg'
+                className='profile-right-top-cover-userImg'
               />
             </div>
-            <div className='profileInfo'>
-              <h4 className='profileInfoName'>{user.username}</h4>
-              <span className='profileInfoDesc'>{user.desc}</span>
+            <div className='profile-right-top-info'>
+              <h4 className='profile-right-top-info-name'>{user.username}</h4>
+              <span className='profile-right-top-info-desc'>{user.desc}</span>
             </div>
           </div>
-          <div className='profileRightBottom'>
+          <div className='profile-right-bottom'>
             <TimeLine username={username} />
             <Rightbar user={user} />
           </div>
         </div>
       </div>
-    </>
+    </StyledProfileDiv>
   );
 };
