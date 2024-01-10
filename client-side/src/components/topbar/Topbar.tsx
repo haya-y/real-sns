f8b317dcc963e5cb17c735fe5f82b6a789f07923
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-11 01:34:40 +0900

    [Mod] LinkのCSSをスタイルドコンポーネントに記載

--- client-side/src/components/topbar/Topbar.tsx
+++ client-side/src/components/topbar/Topbar.tsx
@@ -1,4 +1,4 @@
-import * as MUI from '@mui/icons-material';
+import { Chat as MUIChat, Notifications as MUINotifications, Search as MUISearch } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { StyledTopbarDiv } from './Topbar.styles';
 
@@ -6,24 +6,24 @@ export const Topbar = () => {
   return (
     <StyledTopbarDiv>
       <div className='topbarLeft'>
-        <Link to={'/'} style={{ textDecoration: 'none' }}>
-          <span className='topbarLeft-log'>Real SNS</span>
+        <Link className='topbarLeft-link' to={'/'} style={{}}>
+          <span className='topbarLeft-link-log'>Real SNS</span>
         </Link>
       </div>
       <div className='topbarCenter'>
         <div className='topbarCenter-searchbar'>
-          <MUI.Search className='topbarCenter-searchbar-searchIcon'></MUI.Search>
+          <MUISearch className='topbarCenter-searchbar-searchIcon'></MUISearch>
           <input type='text' className='topbarCenter-searchbar-searchInput' placeholder='探し物は何ですか？' />
         </div>
       </div>
       <div className='topbarRight'>
         <div className='topbarRight-icons'>
           <div className='topbarRight-icons-items'>
-            <MUI.Chat />
+            <MUIChat />
             <span className='topbarRight-icons-items-badge'>10</span>
           </div>
           <div className='topbarRight-icons-items'>
-            <MUI.Notifications />
+            <MUINotifications />
             <span className='topbarRight-icons-items-badge'>20</span>
           </div>
           <img src='/assets/person/1.jpeg' alt='' />
