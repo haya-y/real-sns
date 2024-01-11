0bf866098fb8f5ecd242678f1bd755d385a05778
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-11 09:43:30 +0900

    [Add] sidebarFriendをLinkでラップ

--- client-side/src/components/sidebarFriend/SidebarFriend.tsx
+++ client-side/src/components/sidebarFriend/SidebarFriend.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { PUBLIC_FOLDER } from '../../constants';
 import { User } from '../../types/User.types';
 import { StyledSidebarFriendLi } from './SidebarFriend.styles';
@@ -9,8 +10,10 @@ type Props = {
 export const SidebarFriend = ({ user: { profilePicture, username } }: Props) => {
   return (
     <StyledSidebarFriendLi>
-      <img src={PUBLIC_FOLDER + profilePicture} alt='' className='sidebarFriendImg' />
-      <span className='sidebarFriendName'>{username}</span>
+      <Link className='sidebarFriend-link' to={`/profile/${username}`}>
+        <img src={PUBLIC_FOLDER + profilePicture} alt='profile' className='sidebarFriend-link-img' />
+        <span className='sidebarFriend-link-name'>{username}</span>
+      </Link>
     </StyledSidebarFriendLi>
   );
 };
