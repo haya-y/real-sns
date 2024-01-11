86fa03418f538533a628a2757a6c237d492a9432
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-11 10:06:28 +0900

    [Mod] sidebarのli要素をmapでまとめ

--- client-side/src/components/sidebar/Sidebar.tsx
+++ client-side/src/components/sidebar/Sidebar.tsx
@@ -4,54 +4,32 @@ import { Users } from '../../dummyData';
 import { SidebarFriend } from '../sidebarFriend/SidebarFriend';
 import { StyledSidebarDiv } from './Sidebar.styles';
 
+const muiClassName = 'sidebarWrapper-list-link-item-icon';
+
+const sidebarItems = [
+  { id: 1, to: '/', icon: <MUI.Home className={muiClassName} />, text: 'ホーム' },
+  { id: 2, to: '', icon: <MUI.Search className={muiClassName} />, text: '検索' },
+  { id: 3, to: '', icon: <MUI.Notifications className={muiClassName} />, text: '通知' },
+  { id: 4, to: '', icon: <MUI.MessageRounded className={muiClassName} />, text: 'メッセージ' },
+  { id: 5, to: '', icon: <MUI.Bookmark className={muiClassName} />, text: 'ブックマーク' },
+  // TODO: ↓のURLはダミー
+  { id: 6, to: '/profile/hayate', icon: <MUI.Person className={muiClassName} />, text: 'プロフィール' },
+  { id: 7, to: '', icon: <MUI.Settings className={muiClassName} />, text: '設定' },
+];
+
 export const Sidebar = () => {
   return (
     <StyledSidebarDiv>
       <div className='sidebarWrapper'>
         <ul className='sidebarWrapper-list'>
-          <Link className='sidebarWrapper-list-link' to={'/'} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.Home className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>ホーム</span>
-            </li>
-          </Link>
-          <Link className='sidebarWrapper-list-link' to={''} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.Search className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>検索</span>
-            </li>
-          </Link>
-          <Link className='sidebarWrapper-list-link' to={''} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.Notifications className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>通知</span>
-            </li>
-          </Link>
-          <Link className='sidebarWrapper-list-link' to={''} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.MessageRounded className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>メッセージ</span>
-            </li>
-          </Link>
-          <Link className='sidebarWrapper-list-link' to={''} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.Bookmark className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>ブックマーク</span>
-            </li>
-          </Link>
-          {/* TODO ⇩のProfileページのリンクはダミー */}
-          <Link className='sidebarWrapper-list-link' to={'/profile/hayate'} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.Person className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>プロフィール</span>
-            </li>
-          </Link>
-          <Link className='sidebarWrapper-list-link' to={''} style={{}}>
-            <li className='sidebarWrapper-list-link-item'>
-              <MUI.Settings className='sidebarWrapper-list-link-item-icon' />
-              <span className='sidebarWrapper-list-link-item-text'>設定</span>
-            </li>
-          </Link>
+          {sidebarItems.map(({ id, to, icon, text }) => (
+            <Link className='sidebarWrapper-list-link' to={to} key={id}>
+              <li className='sidebarWrapper-list-link-item'>
+                {icon}
+                <span className='sidebarWrapper-list-link-item-text'>{text}</span>
+              </li>
+            </Link>
+          ))}
         </ul>
         <hr className='sidebarWrapper-line' />
         <ul className='sidebarWrapper-friendList'>
