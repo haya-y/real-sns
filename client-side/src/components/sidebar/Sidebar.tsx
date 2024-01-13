a3ea019d3a8e5780ee02d7a859e42a115d8a3991
Hayate <hayate.bgu.senyo@gmail.com>
2024-01-13 10:52:01 +0900

    [Add] サイドバーのプロフィール遷移でusernameを適用

--- client-side/src/components/sidebar/Sidebar.tsx
+++ client-side/src/components/sidebar/Sidebar.tsx
@@ -1,23 +1,36 @@
 import * as MUI from '@mui/icons-material';
+import { useContext, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { Users } from '../../dummyData';
+import { AuthContext } from '../../redux/AuthContext';
 import { SidebarFriend } from '../sidebarFriend/SidebarFriend';
 import { StyledSidebarDiv } from './Sidebar.styles';
 
 const muiClassName = 'sidebarWrapper-list-link-item-icon';
 
-const sidebarItems = [
-  { id: 1, to: '/', icon: <MUI.Home className={muiClassName} />, text: 'ホーム' },
-  { id: 2, to: '', icon: <MUI.Search className={muiClassName} />, text: '検索' },
-  { id: 3, to: '', icon: <MUI.Notifications className={muiClassName} />, text: '通知' },
-  { id: 4, to: '', icon: <MUI.MessageRounded className={muiClassName} />, text: 'メッセージ' },
-  { id: 5, to: '', icon: <MUI.Bookmark className={muiClassName} />, text: 'ブックマーク' },
-  // TODO: ↓のURLはダミー
-  { id: 6, to: '/profile/hayate', icon: <MUI.Person className={muiClassName} />, text: 'プロフィール' },
-  { id: 7, to: '', icon: <MUI.Settings className={muiClassName} />, text: '設定' },
-];
-
 export const Sidebar = () => {
+  const {
+    state: { user },
+  } = useContext(AuthContext);
+
+  const sidebarItems = useMemo(
+    () => [
+      { id: 1, to: '/', icon: <MUI.Home className={muiClassName} />, text: 'ホーム' },
+      { id: 2, to: '', icon: <MUI.Search className={muiClassName} />, text: '検索' },
+      { id: 3, to: '', icon: <MUI.Notifications className={muiClassName} />, text: '通知' },
+      { id: 4, to: '', icon: <MUI.MessageRounded className={muiClassName} />, text: 'メッセージ' },
+      { id: 5, to: '', icon: <MUI.Bookmark className={muiClassName} />, text: 'ブックマーク' },
+      {
+        id: 6,
+        to: `/profile/${user?.username ?? ''}`,
+        icon: <MUI.Person className={muiClassName} />,
+        text: 'プロフィール',
+      },
+      { id: 7, to: '', icon: <MUI.Settings className={muiClassName} />, text: '設定' },
+    ],
+    [user?.username],
+  );
+
   return (
     <StyledSidebarDiv>
       <div className='sidebarWrapper'>
